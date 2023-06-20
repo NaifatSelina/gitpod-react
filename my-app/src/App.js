77c5b9c0b681f4d697fb8c5e5126806c66d35563
NaifatSelina <naifatselina@icloud.com>
2023-06-20 11:16:47 +0000

    create stateful class component and add props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreeting greeting="I am a stateful class component" name="Naifat" />
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Naifat" age="23" />
     </div>
   );
